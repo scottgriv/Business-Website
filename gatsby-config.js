3fb2e466066ec54d22f2c06da0c5f1bc4b1ae928
Scott Griv <scottgrivner@gmail.com>
2023-10-29 00:26:24 -0400

    Update theme_color in gatsby_config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,10 +31,10 @@ module.exports = {
         name: `Night Owl Labs`,
         short_name: `Night Owl Labs`,
         start_url: `/`,
-        background_color: `#fcdb3a`,
+        background_color: `#0E2F00`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        theme_color: `#fcdb3a`,
+        theme_color: `#0E2F00`,
         display: `minimal-ui`,
         icon: `src/images/icon-rounded.png`, // This path is relative to the root of the site.
       },
