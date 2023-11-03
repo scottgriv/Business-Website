7a5243207df4ec3a6f5a18727e4b0e8b20f52a38
Scott Griv <scottgrivner@gmail.com>
2023-11-03 15:32:12 -0400

    Fixed casing issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 import './Layout.css';
 
 const Layout = ({ children }) => {
