048b292a354975dbeaeef4979d4593317143a21d
Scott Griv <scottgrivner@gmail.com>
2023-11-11 12:55:22 -0500

    Update config

--- src/components/config.js
+++ src/components/config.js
@@ -2,7 +2,7 @@
 // This file is used to control the visibility of the landing page, header, and footer.
 // Optionally, you may want to hide the header or footer if you're using this as a landing page for a new project.
 export const CONFIG = {
-    showLandingPage: true, // Set to false to hide the landing page
-    showHeader: false, // Set to false to hide the header
+    showLandingPage: false, // Set to false to hide the landing page
+    showHeader: true, // Set to false to hide the header
     showFooter: true, // Set to false to hide the footer
   };
