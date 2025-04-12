f4802fd6e99465e16bbac6df1605e9e4cfc67826
Scott Griv <scottgrivner@gmail.com>
2025-04-12 14:27:16 -0400

    Update favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,56 +40,7 @@ module.exports = {
         //theme_color: `#005E1F`, //Lighter Green
         theme_color: `#0E2F00`, //Dark Green
         // display: `minimal-ui`,
-        icons: [
-          {
-            src: `images/favicons/apple-touch-icon-48x48.png`,
-            sizes: `48x48`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-          {
-            src: `/favicons/apple-touch-icon-72x72.png`,
-            sizes: `72x72`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-          {
-            src: `/favicons/apple-touch-icon-96x96.png`,
-            sizes: `96x96`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-          {
-            src: `/favicons/apple-touch-icon-144x144.png`,
-            sizes: `144x144`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-          {
-            src: `/favicons/apple-touch-icon-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-          {
-            src: `/favicons/apple-touch-icon-256x256.png`,
-            sizes: `256x256`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-          {
-            src: `/favicons/apple-touch-icon-384x384.png`,
-            sizes: `384x384`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-          {
-            src: `/favicons/apple-touch-icon-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-            purpose: `any maskable`,
-          },
-        ],
+        icon: `src/images/home-icon.png`, // This path is relative to the root of the site.
         display: `standalone`,
         include_favicon: false,
       },
